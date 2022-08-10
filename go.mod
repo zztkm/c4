ed8dc228060923d5421f2dd5faddec3a30d71206
TakumiTsuruta <ttsuruta@landscape.co.jp>
2022-08-10 12:53:53 +0900

    🔖 v0.0.2

--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module github.com/zztkm/c4
 go 1.19
 
 require (
-	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/gen2brain/beeep v0.0.0-20220518085355-d7852edf42fc // indirect
+	github.com/atotto/clipboard v0.1.4
+	github.com/gen2brain/beeep v0.0.0-20220518085355-d7852edf42fc
+	github.com/rs/zerolog v1.27.0
+)
+
+require (
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
 	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
--- main.go
+++ main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 	"unicode/utf8"
 
 	"github.com/atotto/clipboard"
 	"github.com/gen2brain/beeep"
+	"github.com/rs/zerolog"
 )
 
+var logger zerolog.Logger
+
 func main() {
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	logger = zerolog.New(consoleWriter)
+
 	text, err := clipboard.ReadAll()
 	if err != nil {
-		panic(err)
+		logger.Error().Err(err).Send()
+		os.Exit(1)
 	}
 
 	// 無限ループしながらclipboardが更新されるのを待つ
 	for {
 		now, err := clipboard.ReadAll()
 		if err != nil {
-			panic(err)
+			logger.Error().Err(err).Send()
+			os.Exit(1)
 		}
 
 		if text == now {
@@ -27,6 +36,7 @@ func main() {
 		} else {
 			len := utf8.RuneCountInString(now)
 			msg := fmt.Sprintf("count: %d", len)
+			logger.Info().Str("text", now).Int("count", len).Send()
 			err := beeep.Notify("c4", msg, "")
 			if err != nil {
 				panic(err)
